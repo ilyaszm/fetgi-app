b8c9ae1942ce5dfd158331e7678100b6063ef80f
ilyaszm <zmr.ilyas@gmail.com>
2019-07-09 22:58:52 +0100

    Adding repos page

---
+++ src/components/repos/Repos.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import Header2 from '../layouts/Header2'
+import Footer from '../layouts/Footer'
+import Repo from './Repo'
+
+
+function Repos() {
+
+    useEffect(() => {
+        fetchRepos()
+    }, [])
+
+    const [repos, setRepos] = useState([])
+
+    const fetchRepos = async () => {
+        const data = await fetch('https://api.github.com/search/repositories?q=game+language:javascript&sort=stars&order=desc')
+        const repos = await data.json()
+        console.log(repos.items)
+
+        setRepos(repos.items)
+    }
+
+    return(
+        <div>
+            <Header2 />
+            <h4 style={ LinkStyle }>
+                <Link to="/" className="text-decoration-none">Back To Home</Link>
+            </h4>
+            <hr style={ HrStyle } />
+            {
+                repos.map(repo => (
+                    <Repo
+                        key     = { repo.id }
+                        repo    = { repo }
+                    />
+                ))
+            }
+            <Footer />
+        </div>
+    )
+    
+}
+
+const LinkStyle = {
+    textAlign: "center",
+    padding: "2rem"
+}
+
+const HrStyle = {
+    width: "20rem",
+    marginBottom: "3rem"
+}
+
+export default Repos
