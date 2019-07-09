aa2fef62efed69798c36c2d774edca0023fc10d4
ilyaszm <zmr.ilyas@gmail.com>
2019-07-09 23:02:16 +0100

    Adding the socond header repos page

---
+++ src/components/layouts/Header2.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import '../../assets/css/animate.css'
+import BackgroundImage from '../../assets/images/bg.png'
+
+function Header2() {
+    return (
+        <div>
+            <header style={ HeaderStyle }>
+                <h1 className="animated fadeIn text-center text-uppercase" style={ TitleStyle }>
+                    Trending
+                </h1>
+            </header>
+        </div>
+    )
+}
+
+const HeaderStyle = {
+    width: "100%",
+    minHeight: "280px",
+    background: `url(${BackgroundImage})`,
+    backgroundAttachment: "fixed",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+}
+
+const TitleStyle = {
+    paddingTop: "6rem",
+    color: "#ddd"
+}
+
+export default Header2
